2fc3a57ff04566ca054a550b5e421ce6960acde3
apexethdev <apexethdev@gmail.com>
2024-03-13 12:30:14 +0000

    blob update

--- api/index.tsx
+++ api/index.tsx
@@ -2,6 +2,7 @@ import { Button, Frog, TextInput } from 'frog'
 import { handle } from 'frog/vercel'
 import { abi } from './abi.js'
 
+
 export const app = new Frog({
   assetsPath: '/',
   basePath: '/api',
@@ -12,9 +13,78 @@ app.frame('/', (c) => {
     action: '/finish',
     image: "/blobs.png",
     intents: [
-      <Button.Transaction target="/mint">Mint in Frame</Button.Transaction>,
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+      <Button action='/faq1'>What are Blobs?</Button>,
+      <Button action='/links'>Links</Button>
+    ]
+  })
+})
+
+app.frame('/links', (c) => {
+  return c.res({
+    image: "/blobs.png",
+    intents: [
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
       <Button.Link href="https://opensea.io/collection/onchain-blobs">Opensea</Button.Link>,
-      <Button.Link href="https://mint.fun/base/0xD1fCb4BDBde69F04540d2c52a81AE33aBEA46400">Mint.fun</Button.Link>
+      <Button.Link href="https://mint.fun/base/0xD1fCb4BDBde69F04540d2c52a81AE33aBEA46400">Mint.fun</Button.Link>,
+      <Button action='/'>🏠</Button>
+    ]
+  })
+})
+
+app.frame('/faq1', (c) => {
+  return c.res({
+    image: "/blobs1.png",
+    intents: [
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+      <Button action='/faq2'>➡️</Button>,
+      <Button action='/'>🏠</Button>
+    ]
+  })
+})
+
+app.frame('/faq2', (c) => {
+  return c.res({
+    image: "/blobs2.png",
+    intents: [
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+      <Button action='/faq1'>⬅️</Button>,
+      <Button action='/faq3'>➡️</Button>,
+      <Button action='/'>🏠</Button>
+    ]
+  })
+})
+
+app.frame('/faq3', (c) => {
+  return c.res({
+    image: "/blobs3.png",
+    intents: [
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+      <Button action='/faq2'>⬅️</Button>,
+      <Button action='/faq4'>➡️</Button>,
+      <Button action='/'>🏠</Button>
+    ]
+  })
+})
+
+app.frame('/faq4', (c) => {
+  return c.res({
+    image: "/blobs4.png",
+    intents: [
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+      <Button action='/faq3'>⬅️</Button>,
+      <Button action='/faq5'>➡️</Button>,
+      <Button action='/'>🏠</Button>
+    ]
+  })
+})
+
+app.frame('/faq5', (c) => {
+  return c.res({
+    image: "/blobsend.png",
+    intents: [
+      <Button.Transaction target="/mint">Mint</Button.Transaction>,
+      <Button action='/'>🏠</Button>
     ]
   })
 })
@@ -22,14 +92,11 @@ app.frame('/', (c) => {
 app.frame('/finish', (c) => {
   const { } = c
   return c.res({
-    image: (
-      <div style={{ color: 'white', display: 'flex', fontSize: 40 }}>
-        Frame by Apex777.eth - Rawr!
-      </div>
-    ),
+    image: "/finish.png",
     intents: [
       <Button.Link href="https://opensea.io/collection/onchain-blobs">Blobs Opensea</Button.Link>,
-      <Button action='/'>Start Again</Button>
+      <Button.Link href="https://warpcast.com/apex777">Apex777.eth</Button.Link>,
+      <Button action='/'>🏠</Button>
     ]
   })
 })
