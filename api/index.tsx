0115243c88a74a1ba34c5a05360b15f4c53e09ad
LazyFrank <dev@lazyfrank.xyz>
2024-12-25 00:57:55 -0800

    Updates

--- api/index.tsx
+++ api/index.tsx
@@ -1,115 +1,122 @@
-import { Button, Frog, TextInput } from 'frog'
-import { handle } from 'frog/vercel'
-import { abi } from './abi.js'
-
+import { Button, Frog } from "frog"
+import { handle } from "frog/vercel"
+import { abi } from "./abi.js"
 
 export const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
+  assetsPath: "/",
+  basePath: "/api",
 })
 
-app.frame('/', (c) => {
+app.frame("/", (c) => {
   return c.res({
-    action: '/finish',
+    action: "/finish",
     image: "/blobs.png",
     intents: [
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
-      <Button action='/faq1'>What are Blobs?</Button>,
-      <Button action='/links'>Links</Button>
-    ]
+      <Button action="/faq1">What are Blobs?</Button>,
+      <Button action="/links">Links</Button>,
+    ],
   })
 })
 
-app.frame('/links', (c) => {
+app.frame("/links", (c) => {
   return c.res({
     image: "/blobs.png",
     intents: [
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
-      <Button.Link href="https://opensea.io/collection/onchain-blobs">Opensea</Button.Link>,
-      <Button.Link href="https://mint.fun/base/0xD1fCb4BDBde69F04540d2c52a81AE33aBEA46400">Mint.fun</Button.Link>,
-      <Button action='/'>🏠</Button>
-    ]
+      <Button.Link href="https://opensea.io/collection/onchain-blobs">
+        Opensea
+      </Button.Link>,
+      <Button.Link href="https://mint.fun/base/0xD1fCb4BDBde69F04540d2c52a81AE33aBEA46400">
+        Mint.fun
+      </Button.Link>,
+      <Button action="/">🏠</Button>,
+    ],
   })
 })
 
-app.frame('/faq1', (c) => {
+app.frame("/faq1", (c) => {
   return c.res({
     image: "/blobs1.png",
     intents: [
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
-      <Button action='/faq2'>➡️</Button>,
-      <Button action='/'>🏠</Button>
-    ]
+      <Button action="/faq2">➡️</Button>,
+      <Button action="/">🏠</Button>,
+    ],
   })
 })
 
-app.frame('/faq2', (c) => {
+app.frame("/faq2", (c) => {
   return c.res({
     image: "/blobs2.png",
     intents: [
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
-      <Button action='/faq1'>⬅️</Button>,
-      <Button action='/faq3'>➡️</Button>,
-      <Button action='/'>🏠</Button>
-    ]
+      <Button action="/faq1">⬅️</Button>,
+      <Button action="/faq3">➡️</Button>,
+      <Button action="/">🏠</Button>,
+    ],
   })
 })
 
-app.frame('/faq3', (c) => {
+app.frame("/faq3", (c) => {
   return c.res({
     image: "/blobs3.png",
     intents: [
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
-      <Button action='/faq2'>⬅️</Button>,
-      <Button action='/faq4'>➡️</Button>,
-      <Button action='/'>🏠</Button>
-    ]
+      <Button action="/faq2">⬅️</Button>,
+      <Button action="/faq4">➡️</Button>,
+      <Button action="/">🏠</Button>,
+    ],
   })
 })
 
-app.frame('/faq4', (c) => {
+app.frame("/faq4", (c) => {
   return c.res({
     image: "/blobs4.png",
     intents: [
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
-      <Button action='/faq3'>⬅️</Button>,
-      <Button action='/faq5'>➡️</Button>,
-      <Button action='/'>🏠</Button>
-    ]
+      <Button action="/faq3">⬅️</Button>,
+      <Button action="/faq5">➡️</Button>,
+      <Button action="/">🏠</Button>,
+    ],
   })
 })
 
-app.frame('/faq5', (c) => {
+app.frame("/faq5", (c) => {
   return c.res({
     image: "/blobsend.png",
     intents: [
       <Button.Transaction target="/mint">Mint</Button.Transaction>,
-      <Button action='/'>🏠</Button>
-    ]
+      <Button action="/">🏠</Button>,
+    ],
   })
 })
- 
-app.frame('/finish', (c) => {
-  const { } = c
+
+app.frame("/finish", (c) => {
+  const {} = c
   return c.res({
     image: "/finish.png",
     intents: [
-      <Button.Link href="https://opensea.io/collection/onchain-blobs">Blobs Opensea</Button.Link>,
-      <Button.Link href="https://warpcast.com/apex777">Apex777.eth</Button.Link>,
-      <Button action='/'>🏠</Button>
-    ]
+      <Button.Link href="https://opensea.io/collection/onchain-blobs">
+        Blobs Opensea
+      </Button.Link>,
+      <Button.Link href="https://warpcast.com/apex777">
+        Apex777.eth
+      </Button.Link>,
+      <Button action="/">🏠</Button>,
+    ],
   })
 })
- 
-app.transaction('/mint', (c) => {
+
+app.transaction("/mint", (c) => {
   // Contract transaction response.
   return c.contract({
     abi,
-    chainId: 'eip155:8453',
-    functionName: 'mint',
+    chainId: "eip155:8453",
+    functionName: "mint",
     args: [BigInt(1)],
-    to: '0xD1fCb4BDBde69F04540d2c52a81AE33aBEA46400',
-    value: BigInt(500000000000000)
+    to: "0xD1fCb4BDBde69F04540d2c52a81AE33aBEA46400",
+    value: BigInt(500000000000000),
   })
 })
 
